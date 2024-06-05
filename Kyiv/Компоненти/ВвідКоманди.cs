30215b412289a307a8ffe923e4fe4e3fa4d44091
Andrii Kurdiumov <kant2002@gmail.com>
2024-06-05 20:00:46 +0500

    Додав приклад складання

---
+++ Kyiv/Компоненти/ВвідКоманди.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Kyiv.Компоненти;
+
+public class ВвідКоманди : InputText
+{
+    protected override bool TryParseValueFromString(string? value, out string? result, [NotNullWhen(false)] out string? validationErrorMessage)
+    {
+        result = value;
+        var invalid = value is not null && value.Any(_ => _ < '0' || _ > '7');
+        validationErrorMessage = invalid ? "Не 8-річне число." : null;
+        return !invalid;
+    }
+}
