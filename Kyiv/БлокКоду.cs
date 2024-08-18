d0b92e1e48c98ba32586664c29651e1b571364e9
Andrii Kurdiumov <kant2002@gmail.com>
2024-08-18 17:33:58 +0500

    Зробив щоб декомпілятор працював поверх блоків коду із операцій, а не команд

---
+++ Kyiv/БлокКоду.cs
@@ -0,0 +1,21 @@
+﻿namespace Kyiv;
+
+internal record БлокКоду(int ПочатковаАдреса)
+{
+    public List<Операція> Операції { get; } = [];
+
+    public int ОстанняАдреса => ПочатковаАдреса + Операції.Count - 1;
+
+    public Операція this[int адреса]
+    {
+        get
+        {
+            if (адреса < ПочатковаАдреса || адреса > ОстанняАдреса)
+            {
+                throw new ArgumentOutOfRangeException(nameof(адреса));
+            }
+
+            return Операції[адреса - ПочатковаАдреса];
+        }
+    }
+}
--- Kyiv/Лістінг.cs
+++ Kyiv/Лістінг.cs
@@ -3,6 +3,7 @@
 public class Лістінг
 {
     public List<ОдиницяКоду> ОдиниціКоду { get; } = [];
+    internal List<БлокКоду> БлокиКоду { get; } = [];
 
     public int ПочатковаАдреса => ОдиниціКоду.FirstOrDefault()?.ПочатковаАдреса ?? 0;
 }
