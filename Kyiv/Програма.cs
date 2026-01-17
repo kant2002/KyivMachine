b7ac5311bef4a3c336f78d6703ccb84d7d7ce454
Andrii Kurdiumov <kant2002@gmail.com>
2026-01-17 16:32:58 +0100

    Add localization support on menu

--- Kyiv/Програма.cs
+++ Kyiv/Програма.cs
@@ -1,6 +1,9 @@
 using Kyiv;
 using Мікрософт.АспНетЯдро.Компоненти.Веб;
 using Мікрософт.АспНетЯдро.Компоненти.ВебАсемблі.Хостінг;
+using System.Globalization;
+using Microsoft.JSInterop;
+using Microsoft.Extensions.DependencyInjection;
 
 var будівник = БудівникХостаВебАсемблі.СтворитиЗаЗамовчанням(args);
 будівник.КорневіКомпоненти.Add<Апка>("#app");
@@ -8,5 +11,21 @@ var будівник = БудівникХостаВебАсемблі.Створ
 
 будівник.Сервіси.AddScoped(sp => new HttpClient { BaseAddress = new Uri(будівник.ОточенняХоста.BaseAddress) });
 будівник.Сервіси.AddSingleton<МашинаКиїв>();
+будівник.Сервіси.AddLocalization();
 
-await будівник.Побудувати().ЗапуститиАсінх();
+var хазяїн = будівник.Побудувати();
+const string культураЗаЗамовчанням = "en-US";
+
+var js = хазяїн.Сервіси.GetRequiredService<IJSRuntime>();
+var культураІзНалаштувань = await js.InvokeAsync<string>("blazorCulture.get");
+var культура = CultureInfo.GetCultureInfo(культураІзНалаштувань ?? культураЗаЗамовчанням);
+
+if (культураІзНалаштувань == null)
+{
+    await js.InvokeVoidAsync("blazorCulture.set", культураЗаЗамовчанням);
+}
+
+CultureInfo.DefaultThreadCurrentCulture = культура;
+CultureInfo.DefaultThreadCurrentUICulture = культура;
+
+await хазяїн.ЗапуститиАсінх();
